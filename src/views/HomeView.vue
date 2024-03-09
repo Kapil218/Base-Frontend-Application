1d2700e4b7fc70c09fd585a849c2b1c4b9532f36
kapil singh rathore <kapilsinghrathore218@gmail.com>
2024-03-09 16:58:43 +0530

    m

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,6 +14,7 @@ export default {
   async mounted() {
     this.res = await axios.get("http://127.0.0.1:3000/api/v1/chats/");
     console.log(this.res.data);
+
     // Handle successful response
   },
 };
