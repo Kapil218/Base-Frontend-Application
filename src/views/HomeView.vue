998791599850e35681012049b4e5226d0da3546b
kapil singh rathore <kapilsinghrathore218@gmail.com>
2024-03-10 13:42:07 +0530

    fixes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,39 @@
 <template>
-  <h5>{{ res?.data }}</h5>
+  <div>
+    <h5>{{ res?.data }}</h5>
+    <ul>
+      <li v-for="message in messages" :key="message._id">
+        {{ message.text }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
+import io from "socket.io-client";
+
 export default {
   name: "HomeE",
   data() {
     return {
       res: null,
+      messages: [],
     };
   },
   async mounted() {
+    // Fetch initial data from the backend
     this.res = await axios.get("http://127.0.0.1:3000/api/v1/chats/");
     console.log(this.res.data);
+
+    // Connect to Socket.IO server
+    const socket = io("http://localhost:3000", { transports: ["websocket"] }); // Replace with your Socket.IO server URL
+
+    // Listen for "message" event from Socket.IO server
+    socket.on("message", (data) => {
+      // Add received message to the messages array
+      this.messages.push(data);
+    });
 
     // Handle successful response
   },
